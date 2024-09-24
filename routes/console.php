4e59e3d041aafa6f80cb46eadc51841e32d3dd6b
iyashpal <iyashpal.dev@gmail.com>
2024-09-25 00:58:29 +0530

    Refactor schedule for fetching Reqres users every minute

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(\App\Console\Commands\ReqresUsersCommand::class, ['--fetch-all'])->daily();
+Schedule::command(\App\Console\Commands\ReqresUsersCommand::class, ['--fetch-all'])->everyMinute();
