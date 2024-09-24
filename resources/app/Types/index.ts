e4febd3057648e19bf2f33e323c3c7d44580f327
iyashpal <iyashpal.dev@gmail.com>
2024-09-24 21:01:00 +0530

    update: user model and dashboard page types added.

---
+++ resources/app/Types/index.ts
@@ -0,0 +1,29 @@
+import type {User} from "@/Types/models";
+
+export interface DashboardPageProps {
+    users: PaginationMeta<User[]>
+}
+
+type NullOrString = string | null
+
+export interface PaginationMeta<T> {
+    data: T,
+    first_page_url: NullOrString;
+    prev_page_url: NullOrString,
+    next_page_url: NullOrString,
+    last_page_url: NullOrString,
+    current_page: number;
+    last_page: number,
+    links: PaginationLink[],
+    path: string,
+    per_page: number,
+    from: number,
+    to: number,
+    total: number
+}
+
+export interface PaginationLink {
+    url: NullOrString,
+    label: string,
+    active: boolean
+}
