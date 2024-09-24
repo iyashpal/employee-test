34cd743e0c97af58deb4e5c6e1519523904898a6
iyashpal <iyashpal.dev@gmail.com>
2024-09-24 21:01:57 +0530

    User listing without search added.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
@@ -12,6 +13,8 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return Inertia::render('Dashboard');
+        $users = User::paginate(10);
+
+        return Inertia::render('Dashboard', compact('users'));
     }
 }
--- resources/app/Pages/Dashboard.vue
+++ resources/app/Pages/Dashboard.vue
@@ -1,6 +1,10 @@
-<script setup>
+<script setup lang="ts">
+import {Head, Link} from '@inertiajs/vue3';
+import {DashboardPageProps} from "@/Types";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import {Head} from '@inertiajs/vue3';
+import {DateTime} from "luxon";
+
+defineProps<DashboardPageProps>()
 </script>
 
 <template>
@@ -18,7 +22,7 @@ import {Head} from '@inertiajs/vue3';
                         <div class="sm:flex sm:items-center">
                             <div class="sm:flex-auto">
                                 <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
-                                <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
+                                <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and source.</p>
                             </div>
                             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                                 <div>
@@ -40,32 +44,37 @@ import {Head} from '@inertiajs/vue3';
                                         <thead>
                                         <tr>
                                             <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Name</th>
-                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
                                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
-                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Source</th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Created At</th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Updated At</th>
                                         </tr>
                                         </thead>
                                         <tbody class="divide-y divide-gray-200 bg-white">
-                                        <tr>
+                                        <tr :key="index" v-for="(user, index) in users.data">
                                             <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                                                 <div class="flex items-center">
                                                     <div class="h-11 w-11 flex-shrink-0">
-                                                        <img class="h-11 w-11 rounded-full" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                                        <img class="h-11 w-11 rounded-full" :src="user.avatar_url" :alt="user.name">
                                                     </div>
                                                     <div class="ml-4">
-                                                        <div class="font-medium text-gray-900">Lindsay Walton</div>
-                                                        <div class="mt-1 text-gray-500">lindsay.walton@example.com</div>
+                                                        <div class="font-medium text-gray-900">{{ user.name }}</div>
+                                                        <div class="mt-1 text-gray-500">{{ user.email }}</div>
                                                     </div>
                                                 </div>
                                             </td>
                                             <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                                                <div class="text-gray-900">Front-end Developer</div>
-                                                <div class="mt-1 text-gray-500">Optimization</div>
+                                                <span :class="[user.email_verified_at ? 'bg-green-50 text-green-700 ring-green-600/20' : 'bg-red-50 text-red-700 ring-red-600/20']" class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset">
+                                                    {{ user.email_verified_at ? 'Verified' : 'Un-verified' }}
+                                                </span>
+                                            </td>
+                                            <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{{ user.source }}</td>
+                                            <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                                                {{ DateTime.fromISO(user.created_at).toRelative() }}
                                             </td>
                                             <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                                                <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Active</span>
+                                                {{ DateTime.fromISO(user.updated_at).toRelative() }}
                                             </td>
-                                            <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">Member</td>
                                         </tr>
                                         </tbody>
                                     </table>
@@ -76,17 +85,19 @@ import {Head} from '@inertiajs/vue3';
                             <div class="hidden sm:block">
                                 <p class="text-sm text-gray-700">
                                     Showing
-                                    <span class="font-medium">1</span>
+                                    <span class="font-medium">{{ users.from }}</span>
                                     to
-                                    <span class="font-medium">10</span>
+                                    <span class="font-medium">{{ users.to }}</span>
                                     of
-                                    <span class="font-medium">20</span>
+                                    <span class="font-medium">{{ users.total }}</span>
                                     results
                                 </p>
                             </div>
                             <div class="flex flex-1 justify-between sm:justify-end">
-                                <a href="#" class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Previous</a>
-                                <a href="#" class="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Next</a>
+                                <Link v-if="users.prev_page_url" :href="users.prev_page_url" class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Previous</Link>
+                                <button v-else type="button" disabled class="cursor-not-allowed relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Previous</button>
+                                <Link v-if="users.next_page_url" :href="users.next_page_url" class="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Next</Link>
+                                <button v-else type="button" disabled class="cursor-not-allowed relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Next</button>
                             </div>
                         </nav>
                     </div>
