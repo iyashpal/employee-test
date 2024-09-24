a1efaf78cd7aee16f47839d55b0296952dae3203
iyashpal <iyashpal.dev@gmail.com>
2024-09-24 15:28:25 +0530

    Add configuration file for ReqRes API base URL

---
+++ config/reqres.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Service Base URL
+    |--------------------------------------------------------------------------
+    |
+    | The base URL for the ReqRes API, retrieved from the environment configuration
+    |
+    */
+    'base_url' => env('REQRES_BASE_URL')
+];
