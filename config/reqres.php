80a98fbcd2bbac4ac6f62cebbe3d5e715d0bc087
iyashpal <iyashpal.dev@gmail.com>
2024-09-24 15:33:25 +0530

    Update ReqRes API base URL and records per page configuration

--- config/reqres.php
+++ config/reqres.php
@@ -6,8 +6,21 @@ return [
     | Service Base URL
     |--------------------------------------------------------------------------
     |
-    | The base URL for the ReqRes API, retrieved from the environment configuration
+    | The base URL for the ReqRes API, retrieved from the environment
+    | configuration. Defaults to "https://reqres.in" if not set.
     |
     */
-    'base_url' => env('REQRES_BASE_URL')
+    'base_url' => env('REQRES_BASE_URL', "https://reqres.in"),
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Records Per Page
+    |--------------------------------------------------------------------------
+    |
+    | The number of records to display per page, retrieved from the environment
+    | configuration. Defaults to 10 if not set.
+    |
+    */
+    'per_page_records' => env('REQRES_PER_PAGE_RECORDS', 10)
 ];
