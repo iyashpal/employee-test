a6a89bfe5760f2c120d4cc64b20e5816a513a989
iyashpal <iyashpal.dev@gmail.com>
2024-09-24 18:41:24 +0530

    feat: Add ability to fetch users with pagination via command.

--- app/Console/Commands/ReqresUsersCommand.php
+++ app/Console/Commands/ReqresUsersCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Facades\Reqres;
 use Illuminate\Console\Command;
 
 class ReqresUsersCommand extends Command
@@ -11,7 +12,7 @@ class ReqresUsersCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'reqres:users';
+    protected $signature = 'reqres:sync-users {--fetch-all : Fetch all records pages records.} {--per-page-records=10 : The number of records to pull per page from ReqRes}';
 
     /**
      * The console command description.
@@ -25,6 +26,51 @@ class ReqresUsersCommand extends Command
      */
     public function handle()
     {
-        //
+        $page = 1;
+        $data = collect();
+        $fetchAll = $this->option('fetch-all');
+        $perPageRecords = $this->option('per-page-records');
+
+        $this->info('Fetching users from ReqRes "reqres.in"');
+
+        try {
+            $response = Reqres::getUsers(page: $page, perPage: $perPageRecords);
+
+            // Merge response data to the output data collection.
+            $data->push(...($response->ok() ? $response->json('data') : []));
+
+            // Generate pages range based on response total pages
+            collect(range($page, $response->json('total_pages')))
+
+                // Filter pages which are not fetched and "--fetch-all" option is true
+                ->filter(fn($targetPage) => $targetPage !== $page && $fetchAll)
+
+                // Fetch records for the filtered pages.
+                ->each(function ($page) use ($data, $perPageRecords) {
+                    $response = Reqres::getUsers(page: $page, perPage: $perPageRecords);
+
+                    // Merge response data to the output data collection.
+                    $data->push(...($response->ok() ? $response->json('data') : []));
+                });
+
+            $this->info("Total records fetched: {$data->count()}");
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+
+            return 0;
+        }
+
+        $this->table(
+            ['ID', 'First Name', 'Last Name', 'Email', 'Avatar'],
+            $data->map(fn($item) => [
+                'id' => $item['id'],
+                'first_name' => $item['first_name'],
+                'last_name' => $item['last_name'],
+                'email' => $item['email'],
+                'avatar' => $item['avatar'],
+            ])->toArray(),
+        );
+
+        return 1;
     }
 }
