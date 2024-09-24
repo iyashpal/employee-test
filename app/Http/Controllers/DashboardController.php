040f1610a06574bf50cff3ecb50071c0d170845b
iyashpal <iyashpal.dev@gmail.com>
2024-09-24 21:45:51 +0530

    Refactor user listing search functionality

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class DashboardController extends Controller
 {
@@ -13,7 +14,10 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $users = User::paginate(10);
+        $users = User::when(
+            $request->has('column') && $request->has('search'),
+            fn(Builder $builder) => $builder->whereLike($request->input('column'), "%{$request->input('search')}%"),
+        )->paginate(10)->withQueryString();
 
         return Inertia::render('Dashboard', compact('users'));
     }
