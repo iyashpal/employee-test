cac1d99b9877ee294edbe6d697be3f24d6a9ac76
iyashpal <iyashpal.dev@gmail.com>
2024-09-24 15:28:50 +0530

    Add ReqresServiceContract and ReqresService classes

---
+++ app/Services/ReqresService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\ReqresServiceContract;
+
+class ReqresService implements ReqresServiceContract
+{
+
+    public function __construct()
+    {
+
+    }
+}
